8778cc4a2f28812d2f977d728c7ccb877312f1c5
terr0byte <il.fa201114@yandex.ru>
2024-11-25 11:30:44 +0300

    feat(articleparamsform): add button behaviour

--- src/components/article-params-form/ArticleParamsForm.tsx
+++ src/components/article-params-form/ArticleParamsForm.tsx
@@ -1,13 +1,25 @@
 import { ArrowButton } from 'src/ui/arrow-button';
 import { Button } from 'src/ui/button';
+import { useState, useRef } from 'react';
 
 import styles from './ArticleParamsForm.module.scss';
 
 export const ArticleParamsForm = () => {
+	const [openState, setOpenState] = useState(false);
+	const sideBarRef = useRef<HTMLElement | null>(null);
+	function toggleSidebar() {
+		setOpenState(!openState);
+		if (sideBarRef) sideBarRef.current?.classList.toggle(styles.container_open);
+	}
 	return (
 		<>
-			<ArrowButton isOpen={false} onClick={() => {}} />
-			<aside className={styles.container}>
+			<ArrowButton
+				isOpen={openState}
+				onClick={() => {
+					toggleSidebar();
+				}}
+			/>
+			<aside ref={sideBarRef} className={styles.container}>
 				<form className={styles.form}>
 					<div className={styles.bottomContainer}>
 						<Button title='Сбросить' htmlType='reset' type='clear' />
